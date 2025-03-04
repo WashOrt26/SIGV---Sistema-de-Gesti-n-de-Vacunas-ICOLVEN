7c927d718ce212d90e4ad08846d6177331d95ec4
HernanLopez <hernanf.lopezn@unac.edu.co>
2025-03-03 21:51:02 -0500

    sout de ing civil

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Erika Escribiendo.......");
+        System.out.println("A mi lo que me gusta es la ingenieria civil");
     }
 
 }
