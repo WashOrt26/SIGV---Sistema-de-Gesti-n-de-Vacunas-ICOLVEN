a8feff13c1a19337ac9547d668f1c7ad3a571f60
WashOrt26 <michaelw.ortegah@unac.edu.co>
2025-03-03 21:56:41 -0500

    Creé el lugar donde va a ir el frontend(Ejemplo 2)

--- src/Frontend/Login.java
+++ src/Frontend/Login.java
@@ -3,5 +3,6 @@ package Frontend;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Aquí va a ir el frontend");
+        System.out.println("Ejemplo 2");
     }
 }
