3189d85a7d43045443f4c56e86e4fa8f6c16500b
daniel <daniel.cardenasz@unac.edu.co>
2025-03-03 21:38:21 -0500

    Participantes del proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola somos el equipo de Proyecto integrador:");
-        System.out.println("Michael Whasington Ortega");
+        System.out.println("- Michael Whasington Ortega");
         System.out.println("Hernan Felipe Lopez");
         System.out.println("Erika Fernanda Basto");
         System.out.println("Daniel Cardena Zapata");
