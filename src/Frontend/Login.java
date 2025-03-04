ebc7bf02e0e3cd4752497a18f963caa9d03fdaf2
WashOrt26 <michaelw.ortegah@unac.edu.co>
2025-03-03 22:05:27 -0500

    Ejemplo3

--- src/Frontend/Login.java
+++ src/Frontend/Login.java
@@ -4,5 +4,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Aquí va a ir el frontend");
         System.out.println("Ejemplo 2");
+        System.out.println("Ejemplo3");
     }
 }
